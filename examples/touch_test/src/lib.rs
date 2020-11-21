29062ae1fccd0cbd9e8d12b12c756abe36b2b4f8
khadeem dacosta <khadeem.dacosta@gmail.com>
2020-11-21 17:20:05 -0500

    update touch test
    Former-commit-id: d7f5b30fe24a178234d096e5093520b4365374b6

--- examples/touch_test/src/lib.rs
+++ examples/touch_test/src/lib.rs
@@ -1,13 +1,16 @@
 use fluffl::{
-    FlufflError,
-    window::{*,event_util::*,glow::*},
+    extras::text_writer::*,
+    io::*,
+    window::{event_util::*, glow::*, *},
+    *,
 };
 use std::{
+    cell::{Cell, RefCell},
+    collections::HashMap,
     rc::Rc,
-    cell::{Cell,RefCell},
 };
 
-static CONFIG:&'static str = "
+static CONFIG: &'static str = "
 <window>
 <width>800</width> 
 <height>600</height>
@@ -16,42 +19,85 @@ static CONFIG:&'static str = "
 </window>
 ";
 
-struct AppState{
-
+struct AppState {
+    writer: Option<TextWriter>,
+    touch_positions: HashMap<i32, [f32; 2]>,
 }
-impl AppState{
-    fn new()->Self{
-        Self{
 
+impl AppState {
+    fn new() -> Self {
+        Self {
+            writer: None,
+            touch_positions: HashMap::new(),
         }
     }
 }
-pub async fn fluffl_main() ->Result<(), FlufflError> {
+pub async fn fluffl_main() -> Result<(), FlufflError> {
     let window = FlufflWindow::init(CONFIG).expect("init failed");
-    let state = AppState::new();
     let gl = window.gl();
+    let mut app_state = AppState::new();
 
+    // load font called "plasmatic"
+    let data = load_file!("../../wasm_bins/resources/plasmatic.bcode").expect("load failed");
+    let atlas = HieroAtlas::deserialize(data)
+        .ok()
+        .expect("font unpacked failed");
+    let writer = TextWriter::new(&gl).with_atlas(atlas).build();
+    app_state.writer = Some(writer);
 
-    unsafe{
-        gl.clear_color(0.2, 0.2, 0.2,1.0);
+    unsafe {
+        gl.clear_color(0.2, 0.2, 0.2, 1.0);
         gl.viewport(0, 0, 800, 600);
     }
 
-    FlufflWindow::main_loop(window, state,main_loop);
+    FlufflWindow::main_loop(window, app_state, main_loop);
     Ok(())
 }
 
-async fn main_loop(win_ptr:FlufflWindowPtr,running:Rc<Cell<bool>>,app_state:Rc<RefCell<AppState>>){
+async fn main_loop(
+    win_ptr: FlufflWindowPtr,
+    running: Rc<Cell<bool>>,
+    app_state: Rc<RefCell<AppState>>,
+) {
+    let bounds = win_ptr.window().get_bounds();
     let gl = win_ptr.window().gl();
-    unsafe{
+
+    unsafe {
         gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
     }
-    for event in win_ptr.window_mut().get_events().iter_mut(){
+    let state =  &mut *app_state.borrow_mut();
+    let writer =state.writer.as_mut().unwrap(); 
+
+    for event in win_ptr.window_mut().get_events().iter_mut() {
         match event {
-            EventKind::Quit=>{
+            EventKind::Quit => {
                 running.set(false);
             }
-            _ => ()
+            EventKind::MouseDown {x,y,button_code} => {
+                
+            }
+            EventKind::MouseUp { x,y,button_code} =>{
+
+            }
+            EventKind::MouseMove { x,y,dx,dy}=>{
+            
+            }
+            EventKind::TouchDown { x,y,dx,dy,finger_id} =>{
+                state.touch_positions.insert(finger_id , [x,y]);
+            }
+            EventKind::TouchMove { x,y,dx,dy,finger_id} => {
+                state.touch_positions.insert(finger_id , [x,y]);
+            }
+            EventKind::TouchUp { x,y,dx,dy,finger_id}=>{
+                state.touch_positions.insert(finger_id , [x,y]);
+            }
+            _ => (),
         }
     }
+    //draw text at centered at id positions
+    for (id, &[x,y] ) in state.touch_positions.iter() {
+        let id_str = format!("id:{}",id);
+        let aabb = writer.calc_text_aabb(id_str.as_str(), x, y, 32.);
+        writer.draw_text_line(id_str.as_str(), x-aabb.w/2., y-aabb.h/2., 32., Some(bounds));
+    } 
 }
